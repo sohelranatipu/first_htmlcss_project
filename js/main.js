f1c05665dcf7abccceed8dcc1ce306150c15b861
sohelranatipu <58933178+sohelranatipu@users.noreply.github.com>
2019-12-16 15:22:53 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ window.onload=function(){
+		$('.circleGraphic1').circleGraphic();
+		$('.circleGraphic2').circleGraphic({'color':'#D74680'});
+		$('.circleGraphic3').circleGraphic({'color':'#15C7A8'});
+
+		$('.circleGraphic4').circleGraphic({'color':'#EB7D4B'});
+
+	}
